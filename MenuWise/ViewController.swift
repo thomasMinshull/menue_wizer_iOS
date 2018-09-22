03f85a29cfedbd5ce2b3d7994a96969b6b2772f3
Thomas Minshull <thomasminshull@gmail.com>
2018-09-22 08:59:25 -0700

    initial commit

---
+++ MenuWise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MenuWise
+//
+//  Created by thomas minshull on 2018-09-22.
+//  Copyright © 2018 thomas minshull. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
